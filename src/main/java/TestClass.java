0605447308d5583a4393dc99ef0db8c5d15b1f88
zcc <2972917429@qq.com>
2019-11-26 23:20:01 +0800

    Host branch

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -4,5 +4,6 @@ public static void main(String[] args) {
 	System.out.println("Hello MyEclipse");
 	System.out.println("Eclipse Test");
 	System.out.println("TEST GIT");
+	System.out.println("HOst _List");
 }
 }
