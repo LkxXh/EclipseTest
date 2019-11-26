60211b000e9cceb3e70872652bca56f59d5da390
zcc <2972917429@qq.com>
2019-11-26 23:43:39 +0800

    Host to Master

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -5,5 +5,6 @@ public static void main(String[] args) {
 	System.out.println("Eclipse Test");
 	System.out.println("TEST GIT");
 	System.out.println("HOst _List");
+	System.out.println("validate success to master");
 }
 }
